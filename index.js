a808e6c1438622fb060362e09fb81eb1e92f40e9
Paul <pjackson@gmail.com>
2015-02-16 11:33:44 +0000

    Refactor unknown command handling

--- index.js
+++ index.js
@@ -21,6 +21,18 @@ const
 
 
 // -----------------------------------------------------------------
+//  Command flag
+// -----------------------------------------------------------------
+//  There does not appear to be a nice way in commander to work out
+//  whether a command is valid—there's a catch-all '*' command but
+//  that also outputs in the help; which is not what you would want!
+//  Therefore, this flag simply states whether a command has been
+//  executed; the flag is checked on the 'nextTick'.
+
+var _handled = false;
+
+
+// -----------------------------------------------------------------
 //  Program
 
 program.version(pkg.version);
@@ -28,6 +40,7 @@ program
   .command('login <username>')
   .description('Login to the timesheet system')
   .action(function (username) {
+    _handled = true;
     assert(username);
     tasks.login(username);
   });
@@ -36,6 +49,7 @@ program
   .command('start <taskname> [time]')
   .description('Starts a new task with the name')
   .action(function (taskname, time) {
+    _handled = true;
     assert(taskname);
     tasks.start(taskname, time);
   });
@@ -44,6 +58,7 @@ program
   .command('stop [time]')
   .description('Stops the current task')
   .action(function (time) {
+    _handled = true;
     tasks.stop(time);
   });
 
@@ -51,6 +66,7 @@ program
   .command('cancel')
   .description('Cancels the current task')
   .action(function () {
+    _handled = true;
     tasks.cancel();
   });
 
@@ -58,6 +74,7 @@ program
   .command('add <taskname> <start> <duration>')
   .description('Add a new task with an arbitrary start date and duration')
   .action(function (taskname, start, duration) {
+    _handled = true;
     assert(taskname);
     assert(start);
     assert(duration);
@@ -69,6 +86,7 @@ program
   .command('current')
   .description('Display\'s the current task information')
   .action(function () {
+    _handled = true;
     reports.current();
   });
 
@@ -76,6 +94,7 @@ program
   .command('last')
   .description('Display\'s the last completed task')
   .action(function () {
+    _handled = true;
     console.log(arguments);
   });
 
@@ -83,6 +102,7 @@ program
   .command('today')
   .description('Display\'s all the tasks for today')
   .action(function () {
+    _handled = true;
     console.log(arguments);
   });
 
@@ -91,6 +111,7 @@ program
   .description('Display\'s all the tasks for current week')
   .option('-p, --prev [number]', 'Specify the number of weeks to go back', 1)
   .action(function (options) {
+    _handled = true;
     assert(options);
 
     console.log(arguments);
@@ -102,6 +123,7 @@ program
   .option('-p, --prev [number]', 'Specify the number of months to go back', 1)
   .option('-c, --csv', 'Output in CSV format')
   .action(function (options) {
+    _handled = true;
     assert(options);
 
     console.log(arguments);
@@ -113,8 +135,11 @@ if (program.args.length === 0) {
   program.help();
   process.exit(1);
 }
-if (typeof program.args[0] === 'string') {
-  console.log('\n  !! Unknown command !!');
-  program.help();
-  process.exit(1);
-}
+
+process.nextTick(function () {
+  if (!_handled) {
+    console.log('\n !! Unknown command !! ');
+    program.help();
+    process.exit();
+  }
+});
